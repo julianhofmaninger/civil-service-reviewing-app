bf4db85df1542c2e5120b4ff9e6435d04d6ad97a
Maximilian Hollnbuchner <maximilianhollnbuchner@gmail.com>
2024-02-04 21:21:06 +0100

    Added Username.cs, represents a user's imaginary name

---
+++ src/backend/CIVS/Domain/Common/Username.cs
@@ -0,0 +1,11 @@
+﻿namespace Domain.Common;
+
+public readonly record struct Username
+{
+    private readonly string _value;
+    public Username(string username)
+    {
+        ArgumentException.ThrowIfNullOrEmpty(username);
+        _value = username;
+    }
+}
