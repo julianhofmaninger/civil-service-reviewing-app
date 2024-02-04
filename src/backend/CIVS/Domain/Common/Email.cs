bbb899b4fe7c896dd25f73b3553f75b55b66131a
Maximilian Hollnbuchner <maximilianhollnbuchner@gmail.com>
2024-02-04 21:20:36 +0100

    Added Email.cs

---
+++ src/backend/CIVS/Domain/Common/Email.cs
@@ -0,0 +1,49 @@
+﻿using System.Globalization;
+using System.Text.RegularExpressions;
+
+namespace Domain.Common;
+
+public readonly record struct Email
+{
+    private readonly string _value;
+    
+    public Email(string email)
+    {
+        if (!IsValidEmail(email))
+            throw new InvalidDataException();
+        _value = email;
+    }
+
+    private bool IsValidEmail(string email)
+    {
+        if (string.IsNullOrWhiteSpace(email))
+            return false;
+
+        try
+        {
+            // Normalize the domain
+            email = Regex.Replace(email, @"(@)(.+)$", DomainMapper,
+                RegexOptions.None, TimeSpan.FromMilliseconds(200));
+
+            // Examines the domain part of the email and normalizes it.
+            string DomainMapper(Match match)
+            {
+                // Use IdnMapping class to convert Unicode domain names.
+                var idn = new IdnMapping();
+
+                // Pull out and process domain name (throws ArgumentException on invalid)
+                var domainName = idn.GetAscii(match.Groups[2].Value);
+
+                return match.Groups[1].Value + domainName;
+            }
+            
+            return Regex.IsMatch(email,
+                @"^[^@\s]+@[^@\s]+\.[^@\s]+$",
+                RegexOptions.IgnoreCase, TimeSpan.FromMilliseconds(250));
+        }
+        catch
+        {
+            return false;
+        }
+    }
+}
