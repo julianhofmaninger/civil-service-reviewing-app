b3379d97b1f95b0af4c3ee8387340e9cf67d32be
Maximilian Hollnbuchner <maximilianhollnbuchner@gmail.com>
2024-02-04 23:58:42 +0100

    Added Address value object

---
+++ src/backend/CIVS/Domain/Common/Address.cs
@@ -0,0 +1,11 @@
+﻿namespace Domain.Common;
+
+public readonly struct Address
+{
+    public string AddressName { get; init; }
+    public string ZipCode { get; init; }
+    public string City { get; init; }
+    public string AreaCode { get; init; }
+    public string Region { get; init; }
+    public string DistrictCode { get; init; }
+}
