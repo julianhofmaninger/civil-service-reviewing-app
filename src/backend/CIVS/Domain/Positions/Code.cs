aa4a610fb33b72de61a10dbf92e5ff58d9fff9a4
Maximilian Hollnbuchner <maximilianhollnbuchner@gmail.com>
2024-02-04 23:59:11 +0100

    Added Code.cs value-object to positions

---
+++ src/backend/CIVS/Domain/Positions/Code.cs
@@ -0,0 +1,12 @@
+﻿namespace Domain.Positions;
+
+public readonly struct Code
+{
+    private readonly int _code;
+    public Code(int code)
+    {
+        if (code.ToString().Length != 5)
+            throw new InvalidDataException("Department codes are 5 digits long!");
+        _code = code;
+    }
+}
