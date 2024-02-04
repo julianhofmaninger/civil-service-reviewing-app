59ef44883499f0c73ceb49094e3335dd9e99fd25
Maximilian Hollnbuchner <maximilianhollnbuchner@gmail.com>
2024-02-04 23:58:29 +0100

    Added Slot.cs model and the corresponding id

---
+++ src/backend/CIVS/Domain/Slots/Slot.cs
@@ -0,0 +1,12 @@
+namespace Domain.Slots;
+
+public sealed record Slot
+{
+    private Slot() { }
+    
+    public SlotId Id { get; init; } = SlotId.New();
+    public DateOnly Date { get; init; }
+    public int Capacity { get; init; }
+    public int Available { get; init; }
+    public int Used { get; init; }
+}
---
+++ src/backend/CIVS/Domain/Slots/SlotId.cs
@@ -0,0 +1,5 @@
+using StrongIDs;
+
+namespace Domain.Slots;
+
+public readonly partial record struct SlotId : IEntityId<SlotId>;
