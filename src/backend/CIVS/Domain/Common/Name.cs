4c2cc3c63c2dc2330d5140aab46c83879019c102
Maximilian Hollnbuchner <maximilianhollnbuchner@gmail.com>
2024-02-04 21:20:47 +0100

    Added Name.cs represents a users real name

---
+++ src/backend/CIVS/Domain/Common/Name.cs
@@ -0,0 +1,18 @@
+﻿namespace Domain.Common;
+
+public readonly record struct Name
+{
+    public Name(string firstname, string lastname)
+    {
+        Firstname = firstname;
+        Lastname = lastname;
+    }
+    
+    public string Firstname { get; init; }
+    public string Lastname { get; init; }
+    public string Fullname => $"{Firstname} {Lastname}";
+
+    public static Name Empty => new Name(string.Empty, string.Empty);
+
+    public bool HasValue => !string.IsNullOrWhiteSpace(Fullname);
+}
