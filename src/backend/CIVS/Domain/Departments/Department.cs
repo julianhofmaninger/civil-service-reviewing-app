c7f7bf54313d160ca4f7c04afe91b7a2d37eabc1
Maximilian Hollnbuchner <maximilianhollnbuchner@gmail.com>
2024-02-05 00:00:15 +0100

    Added Department.cs model and the corresponding id

---
+++ src/backend/CIVS/Domain/Departments/Department.cs
@@ -0,0 +1,26 @@
+﻿namespace Domain.Departments;
+
+public sealed record Department
+{
+    public DepartmentId Id { get; init; } = DepartmentId.New();
+    public required string Code { get; init; }
+    public required string Title { get; init; }
+
+    public Department(string code, string title) : base()
+    {
+        Code = code;
+        Title = title;
+    }
+    
+    private Department() { }
+
+    public static Department None => 
+        new Department
+        {
+            Code = string.Empty,
+            Title = string.Empty
+        };
+
+    public bool HasValue => !string.IsNullOrWhiteSpace(Code) && 
+                            !string.IsNullOrWhiteSpace(Title);
+}
---
+++ src/backend/CIVS/Domain/Departments/DepartmentId.cs
@@ -0,0 +1,5 @@
+using StrongIDs;
+
+namespace Domain.Departments;
+
+public readonly partial record struct DepartmentId : IEntityId<DepartmentId>;
