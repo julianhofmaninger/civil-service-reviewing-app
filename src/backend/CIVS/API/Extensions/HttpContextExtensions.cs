bd2f3713c610a504969d8a3cbff5805989f5f3a4
Maximilian Hollnbuchner <maximilianhollnbuchner@gmail.com>
2024-02-04 20:54:27 +0100

    Added http-extensions to get the signed in users id and email.

---
+++ src/backend/CIVS/API/Extensions/HttpContextExtensions.cs
@@ -0,0 +1,18 @@
+﻿using System.Security.Claims;
+
+namespace API.Extensions;
+
+public static class HttpContextExtensions
+{
+    public static Guid GetUserId(this HttpContext httpContext)
+    {
+        var value = httpContext.User.FindFirstValue(ClaimTypes.NameIdentifier);
+        return Guid.TryParse(value, out var userId) ? userId : Guid.Empty; 
+    }
+
+    public static string GetEmail(this HttpContext httpContext)
+    {
+        var value = httpContext.User.FindFirstValue(ClaimTypes.Email);
+        return value ?? string.Empty;
+    }
+}
