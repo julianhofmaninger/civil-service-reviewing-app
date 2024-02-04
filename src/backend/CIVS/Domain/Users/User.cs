924b88e350e7816eb394443389e8015b8542d2ea
Maximilian Hollnbuchner <maximilianhollnbuchner@gmail.com>
2024-02-04 21:21:21 +0100

    Added User.cs

---
+++ src/backend/CIVS/Domain/Users/User.cs
@@ -0,0 +1,12 @@
+﻿using Domain.Common;
+
+namespace Domain.Users;
+
+public record User
+{
+    public UserId Id { get; init; }
+    public Email Email { get; init; }
+    public DateTime EmailConfirmedAt { get; init; }
+    public Name Name { get; set; } 
+    public Username Username { get; set; }
+}
