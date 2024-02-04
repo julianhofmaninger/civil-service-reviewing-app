4836d33557a9e905573bac3424578d8496318052
Maximilian Hollnbuchner <maximilianhollnbuchner@gmail.com>
2024-02-04 23:59:47 +0100

    Added Contact.cs value object to Postions

---
+++ src/backend/CIVS/Domain/Positions/Contact.cs
@@ -0,0 +1,11 @@
+﻿using Domain.Common;
+
+namespace Domain.Positions;
+
+public readonly struct Contact
+{
+    public Email Email { get; init; }
+    public string Name { get; init; }
+    public string Phone { get; init; }
+    public string Homepage { get; init; }
+}
