ca2fdac025ffd68217edcd17f0e48083c14496e6
Maximilian Hollnbuchner <maximilianhollnbuchner@gmail.com>
2024-02-05 21:07:52 +0100

    Updated Code.cs, init of Value now validates the code

--- src/backend/CIVS/Domain/Positions/Code.cs
+++ src/backend/CIVS/Domain/Positions/Code.cs
@@ -2,11 +2,16 @@
 
 public readonly struct Code
 {
-    private readonly int _code;
-    public Code(int code)
+    private readonly int _value;
+
+    public int Value
     {
-        if (code.ToString().Length != 5)
-            throw new InvalidDataException("Department codes are 5 digits long!");
-        _code = code;
+        get => _value;
+        init
+        {
+            if (value.ToString().Length != 5)
+                throw new InvalidDataException("Department codes are 5 digits long!");
+            _value = value;
+        }
     }
 }
